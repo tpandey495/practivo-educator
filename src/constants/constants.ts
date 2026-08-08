44591a943b0acdaf63af3f9855837915c398ad05
Tarun Pandey <tpandey495@gmail.com>
2026-08-08 17:59:34 +0530

    removed dist from git tracking

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,10 +6,3 @@ export const AUTH_URL: string = BASE_URL + "/v1/auth";
 export const SERVER_URL: string = BASE_URL + "/v1";
 export const UPLOADS: string = BASE_URL + "/";
 
-// for prod env
-// const API_BASE = import.meta.env.VITE_API_BASE_URL ?? "https://backend.practivo.com";
-// export const BASE_URL: string = API_BASE;
-// export const LOCAL_BASE_URL: string = API_BASE;
-// export const AUTH_URL: string = BASE_URL + "/api/v1/auth";
-// export const SERVER_URL: string = BASE_URL + "/api/v1";
-// export const UPLOADS: string = BASE_URL + "/";
