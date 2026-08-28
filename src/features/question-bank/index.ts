16b0206dafa4ac5ece3c304c23380c4e9a5547a3
Tarun Pandey <tpandey495@gmail.com>
2026-08-28 13:32:33 +0530

    removed question bank edit

--- src/features/question-bank/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as QuestionBankComponent } from './components/QuestionBank';
-export { default as AddNewQuestionCard } from './components/AddNewQuestionCard';
-export { default as QuestionCard } from './components/QuestionCard';
-export type { QuestionBank as QuestionBankType, QuestionBankFilters, CreateQuestionBankData } from './types';
